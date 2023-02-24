d2c50369c3b1a03a25e06c3ac71628ce478c1505
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2023-02-24 17:54:00 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,14 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.254/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#更换插件名称
+sed -i 's/ShadowSocksR Plus+/科学上网/g' openwrt/package/feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/("iStore"),/("应用中心"),/g' openwrt/package/feeds/small8/luci-app-store/luasrc/controller/store.lua
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
