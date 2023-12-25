406c59392c2835abee0974ed1060b0bcce922264
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2023-12-25 22:03:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# 自定义
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/10.1.1.254/g' openwrt/package/base-files/files/bin/config_generate
+#1. 修改默认登录地址
+sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#2. 修改默认登录密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
 
 #更换插件名称
-#sed -i 's/ShadowSocksR Plus+/科学上网/g' openwrt/package/feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-#sed -i 's/("iStore"),/("应用中心"),/g' openwrt/package/feeds/small8/luci-app-store/luasrc/controller/store.lua
+#sed -i 's/ShadowSocksR Plus+/科学上网/g' ./package/feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/("iStore"),/("应用中心"),/g' ./package/feeds/small8/luci-app-store/luasrc/controller/store.lua
+
+#修改内核版本
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
