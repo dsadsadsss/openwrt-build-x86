39332486487c836f8c0a588c6d14ff33a8b93179
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-01-14 23:28:44 +0800

    Update preset-clash-core.sh

--- customize/preset-clash-core.sh
+++ customize/preset-clash-core.sh
@@ -9,9 +9,9 @@
 #=================================================
 
 echo -e "预置Clash内核"
-mkdir -p luci-app-openclash/root/etc/openclash/core
-core_path="luci-app-openclash/root/etc/openclash/core"
-goe_path="luci-app-openclash/root/etc/openclash"
+mkdir -p ./root/etc/openclash/core
+core_path="./root/etc/openclash/core"
+goe_path="./root/etc/openclash"
 
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep "v3" )
