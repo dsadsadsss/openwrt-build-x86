1faf09f7bad2e738504738bca2cc0b239396b5a9
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-01-17 22:45:28 +0800

    Create immortalwrt_x86_64_customize.sh

---
+++ customize/immortalwrt_x86_64_customize.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#=================================================
+# 自定义
+#=================================================
+#1. 修改默认登录地址
+sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
+
+#2. 修改默认登录密码
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' ./package/base-files/files/etc/shadow
+
+#更换插件名称
+#sed -i 's/ShadowSocksR Plus+/科学上网/g' ./package/feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+sed -i 's/("iStore"),/("应用中心"),/g' ./package/feeds/small8/luci-app-store/luasrc/controller/store.lua
+
+#修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+#sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+
+# 导入openclash核心
+mkdir -p package/feeds/small8/luci-app-openclash/root/etc/openclash/core
+cp ../customize/clash package/feeds/small8/luci-app-openclash/root/etc/openclash/core
+cp ../customize/clash_meta package/feeds/small8/luci-app-openclash/root/etc/openclash/core
+cp ../customize/clash_tun package/feeds/small8/luci-app-openclash/root/etc/openclash/core
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > package/feeds/small8/luci-app-openclash/root/etc/openclash/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > package/feeds/small8/luci-app-openclash/root/etc/openclash/GeoSite.dat
+chmod +x package/feeds/small8/luci-app-openclash/root/etc/openclash/core/clash*
