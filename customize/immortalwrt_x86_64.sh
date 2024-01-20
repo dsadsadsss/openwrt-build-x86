35d83527dd38febed7e75be9acefb49f9f8bc0e4
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-01-20 16:15:42 +0800

    Update immortalwrt_x86_64.sh

--- customize/immortalwrt_x86_64.sh
+++ customize/immortalwrt_x86_64.sh
@@ -16,15 +16,22 @@ sed -i 's/("iStore"),/("应用中心"),/g' ./package/feeds/small8/luci-app-store
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
 # 替换luci-theme-design
 rm -rf package/feeds/small8/luci-theme-design && git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/feeds/small8/luci-theme-design
 
 # 删除冲突插件
 rm -rf feeds/packages/net/{alist,adguardhome,brook,gost,mosdns,redsocks*,smartdns,trojan*,v2ray*,xray*}
 
+#替换 luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-netdata
+git clone --depth=1 https://github.com/Jason6111/luci-app-netdata feeds/luci/applications/luci-app-netdata
+
 # 替换diy文件
 mkdir -p files/usr/lib/lua/luci/view/admin_status
 cp ../customize/diy/index.htm files/usr/lib/lua/luci/view/admin_status/index.htm
 
-./scripts/feeds update -a
+
 ./scripts/feeds install -a
