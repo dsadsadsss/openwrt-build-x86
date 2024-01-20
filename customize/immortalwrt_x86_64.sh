69b77f30ffdefb3e04ede9e2338be2175eed2705
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-01-20 14:59:16 +0800

    Update immortalwrt_x86_64.sh

--- customize/immortalwrt_x86_64.sh
+++ customize/immortalwrt_x86_64.sh
@@ -2,25 +2,29 @@
 #=================================================
 # 自定义
 #=================================================
-#1. 修改默认登录地址
+# 修改默认登录地址
 sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
 
-#2. 修改默认登录密码
+# 修改默认登录密码
 #sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' ./package/base-files/files/etc/shadow
 
-#更换插件名称
+# 更换插件名称
 #sed -i 's/ShadowSocksR Plus+/科学上网/g' ./package/feeds/small8/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 sed -i 's/("iStore"),/("应用中心"),/g' ./package/feeds/small8/luci-app-store/luasrc/controller/store.lua
 
-#修改内核版本
+# 修改内核版本
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # 替换luci-theme-design
 rm -rf package/feeds/small8/luci-theme-design && git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/feeds/small8/luci-theme-design
 
-#删除冲突插件
+# 删除冲突插件
 rm -rf feeds/packages/net/{alist,adguardhome,brook,gost,mosdns,redsocks*,smartdns,trojan*,v2ray*,xray*}
+
+# 替换diy文件
+mkdir -p files/usr/lib/lua/luci/view/admin_status
+cp ../customize/diy/index.htm files/usr/lib/lua/luci/view/admin_status/index.htm
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
