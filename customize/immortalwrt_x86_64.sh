944ba239d32c8e98bb1e5c44dd85d9e5603ff871
lmxslpc <101076983+lmxslpc@users.noreply.github.com>
2024-01-21 08:38:33 +0800

    Update immortalwrt_x86_64.sh

--- customize/immortalwrt_x86_64.sh
+++ customize/immortalwrt_x86_64.sh
@@ -3,7 +3,7 @@
 # 自定义
 #=================================================
 # 修改默认登录地址
-sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.1.1.254/g' ./package/base-files/files/bin/config_generate
 
 # 修改默认登录密码
 #sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' ./package/base-files/files/etc/shadow
